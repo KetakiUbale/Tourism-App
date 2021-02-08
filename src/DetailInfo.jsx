db4bb71213fb736f5d0a845defacad617acfee78
ubaleketaki@gmail.com <ubaleketaki@gmail.com>
2021-02-08 11:55:00 +0530

    Initial Commit

---
+++ src/DetailInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import {Switch, Route, Redirect} from "react-router-dom";
+const DetailInfo = () => {
+    return(
+        <>
+<h1>Hello world</h1>
+
+      </>
+    );
+};
+export default DetailInfo;
