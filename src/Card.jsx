8426cf5659e930a5940a59c5ed0ec5b56b004452
ubaleketaki@gmail.com <ubaleketaki@gmail.com>
2021-02-28 18:17:03 +0530

    Add package added

--- src/Card.jsx
+++ src/Card.jsx
@@ -12,9 +12,10 @@ const Card = (props) => {
                                <h5 className="card-title font-weight-bold">{props.title}</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <NavLink to="DetailInfo" className="btn btn-primary">In Detail Info
+                                
                                 </NavLink>
                                 <NavLink className="btn btn-warning mr-1" to="/edit/1">Edit</NavLink>
-                      <Button color="danger">Delete</Button>
+                                  <Button color="danger">Delete</Button>
                                   </div>
                               </div>
                       </div>
